a1261392d83a5fe093bf69c4d4a09df39c960c00
zuhye <mari5669@naver.com>
2022-12-13 16:22:32 +0900

    set: mysql 연결  및 sequalize init, 설정
    mysql 외부에서 연결되는지 확인 중, sequalize init을 통해 파일 및 폴더
    생성

---
+++ server/models/Users.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataTypes) => {
+    const Users = sequelize.define("Users", {
+        id: {
+            type: DataTypes.UUID,
+            defaultVlue: DataTypes.UUIDV4,
+            primaryKey: true,
+            comment: "고유번호 UUID"
+        },
+        email: {
+            type: DataTypes.STRING(100),
+            validate: {
+                isEmail: true,
+            },
+            comment: "이메일",
+        }, 
+        password: {
+            type: DataTypes.STRING(60),
+            comment: "비밀번호"
+        },
+        name: {
+            type: DataTypes.STRING(100),
+            comment: "이름"
+        },
+        phone_number: {
+            type: DataTypes.STRING(72),
+            comment: "전화번호"
+        },
+        likefarms: {
+            type: DataTypes.INTEGER(),
+            comment: "관심농장"
+        },
+    }, {
+        charset: "utf8",
+        collate: "utf8_general_ci",
+        tableName: "Users",
+        timestamps: true,
+        paranoid: true,
+    });
+
+    return Users;
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const {sequelize} =  require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,6 +17,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (req, res) => {
     res.send('Server Response Success');
 })
+
+sequelize.sync({force: false})
+.then(()=> {
+  console.log("데이터베이스 연결 성공 !!")
+})
+.catch((err)=> {
+  console.error(err);
+});
 
 
 app.listen(PORT, () => {
