91eca59be97b266473bf73b08d68928c44e64631
Young-yoii <fhqht4545@gmail.com>
2022-12-15 14:31:41 +0900

    Fix : style 수정

--- client/src/pages/UserPage/MyPage.jsx
+++ client/src/pages/UserPage/MyPage.jsx
@@ -1,76 +1,164 @@
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`
+const StyledTitle = styled.span`
+  font-size: 2rem;
+  font-weight: bold;
+  position: relative;
+
+  &::after {
+    content:'';
+    width: 100%;
+    height: 2px;
+    background-color:lightgray;
+    display: block;
+    position: absolute;
+  }
+`
+const StyledInfoTitle = styled.p`
+  font-weight: 700;
+`
+
+const StyledUserInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  position: relative;
+  padding-bottom: 2%;
+
+  &::after {
+    content:'';
+    width: 100%;
+    height: 2px;
+    background-color:lightgray;
+    display: block;
+    position: absolute;
+    bottom: 0;
+  }
+`
+const StyledInfoSubmit = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  flex-direction: column;
+
+  > input {
+    border:1px solid black;
+    border-radius: 10px;
+    padding: 5%;
+    margin-bottom: 4%;
+  }
+
+  > button {
+    background: lightgray;
+    border:none;
+    border-radius:10px;
+    padding: 5% 10%;
+  }
+`
+const StyledButton = styled.button`
+  border: none;
+  position: relative;
+  background: none;
+  text-decoration: underline;
+`
 
 const MyPage = () => {
-    const user = {
-        name : '홍길동',
-        tel: '010-1234-5678',
-        email: '1234@test.com',
-        password: '123456',
-    }
-    const [userInfo, setUserInfo] = useState(user.name);
-    const [change, setChange] = useState(false)
+  const user = {
+    name : '홍길동',
+    tel: '010-1234-5678',
+    email: '1234@test.com',
+    password: '123456',
+  }
 
-    const handleInfoChangeBtn = () => {
-        setChange(current => !current)
-    }
+  const [userInfo, setUserInfo] = useState({
+    name: `${user.name}`,
+    tel: `${user.tel}`,
+    email: `${user.email}`,
+    password: `${user.password}`
+  }); 
 
-    const handleInfoChange = (event) => {
-        setUserInfo(event.target.value)
-    }
+  const {name, tel, email, password} = userInfo;
 
-    return (
-        <>
-            <h1>내 정보 관리</h1>
-            <div>
-                <div>
-                    <p>이름</p>
-                    <div>
-                        <p>{!change ? `${user.name}` : (
-                            <><input type="text" value={userInfo} onChange={handleInfoChange} />
-                            <button>저장</button>
-                            </>
-                        )}</p>
-                        <button onClick={handleInfoChangeBtn}>{!change ? '수정' : '취소'}</button>
-                    </div>
-                </div>
-                <div>
-                    <p>전화번호</p>
-                    <div>
-                        <p>{!change ? `${user.name}` : (
-                            <><input type="text" value={userInfo} onChange={handleInfoChange} />
-                            <button>저장</button>
-                            </>
-                        )}</p>
-                        <button onClick={handleInfoChangeBtn}>{!change ? '수정' : '취소'}</button>
-                    </div>
-                </div>
-                <div>
-                    <p>이메일</p>
-                    <div>
-                        <p>{!change ? `${user.name}` : (
-                            <><input type="text" value={userInfo} onChange={handleInfoChange} />
-                            <button>저장</button>
-                            </>
-                        )}</p>
-                        <button onClick={handleInfoChangeBtn}>{!change ? '수정' : '취소'}</button>
-                    </div>
-                </div>
-                <div>
-                    <p>비밀번호</p>
-                    <div>
-                        <p>{!change ? `${user.name}` : (
-                            <><input type="text" value={userInfo} onChange={handleInfoChange} />
-                            <button>저장</button>
-                            </>
-                        )}</p>
-                        <button onClick={handleInfoChangeBtn}>{!change ? '비밀번호 재설정' : '취소'}</button>
-                    </div>
-                </div>
+  const handleInfoChange = (e) => {
+    const { value, name } = e.target; 
+      setUserInfo({
+        ...userInfo, 
+        [name]: value 
+      });
+  };
 
-            </div>
-        </>
+  const [change, setChange] = useState(false)
 
-    )
+  const handleInfoChangeBtn = () => {
+    setChange(current => !current)
+  }
+
+  return (
+    <Container>
+      <StyledTitle>내 정보 관리</StyledTitle>
+          <div>
+            <StyledInfoTitle>이름</StyledInfoTitle>
+              <StyledUserInfo>
+                <StyledInfoSubmit>
+                  {!change ? `${name}` : (
+                    <>
+                      <input name="name" type="text" value={name} onChange={handleInfoChange} />
+                      <button>저장하기</button>
+                    </>
+                  )}
+                </StyledInfoSubmit>
+                <StyledButton onClick={handleInfoChangeBtn}>{!change ? '수정' : '취소'}</StyledButton>
+              </StyledUserInfo>
+          </div>
+          <div>
+              <StyledInfoTitle>전화번호</StyledInfoTitle>
+              <StyledUserInfo>
+                <StyledInfoSubmit>
+                  {!change ? `${tel}` : (
+                    <>
+                      <input name="tel" type="text" value={tel} onChange={handleInfoChange} />
+                      <button>저장하기</button>
+                    </>
+                  )}
+                </StyledInfoSubmit>
+                <StyledButton onClick={handleInfoChangeBtn}>{!change ? '수정' : '취소'}</StyledButton>
+              </StyledUserInfo>
+          </div>
+          <div>
+            <StyledInfoTitle>이메일</StyledInfoTitle>
+            <StyledUserInfo>
+              <StyledInfoSubmit>
+                {!change ? `${email}` : (
+                  <>
+                    <input name="email" type="text" value={email} onChange={handleInfoChange} />
+                    <button>저장하기</button>
+                  </>
+                )}
+              </StyledInfoSubmit>
+              <StyledButton onClick={handleInfoChangeBtn}>{!change ? '수정' : '취소'}</StyledButton>
+            </StyledUserInfo>
+          </div>
+          <div>
+            <StyledInfoTitle>비밀번호</StyledInfoTitle>
+            <StyledUserInfo>
+              <StyledInfoSubmit>
+                {!change ? `${password}` : (
+                  <>
+                    <input name="password" type="text" value={password} onChange={handleInfoChange} />
+                    <button>저장하기</button>
+                  </>
+                )}
+              </StyledInfoSubmit>
+              <StyledButton onClick={handleInfoChangeBtn}>{!change ? '비밀번호 재설정' : '취소'}</StyledButton>
+            </StyledUserInfo>
+          </div>
+    </Container>
+  )
 }
 
 export default MyPage;
--- client/src/styles/GlobalStyle.jsx
+++ client/src/styles/GlobalStyle.jsx
@@ -1,30 +1,29 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-    * {
-        box-sizing: border-box;
+  * {
+      box-sizing: border-box;
     }
 
-    html {
-        font-size: 100%;
-    }
+  html {
+      font-size: 100%;
+  }
 
-    body {
-        margin: 0;
-        padding: 0;
-        min-height: 100vh;
-        min-width: 100vw;
-    }
+  body {
+    margin: 0;
+    padding: 0;
+    min-height: 100vh;
+    min-width: 100vw;
+  }
 
-    ul, li {
-        list-style: none;
-    }
+  ul, li {
+    list-style: none;
+  }
 
-    a {
-        text-decoration: none;
-        display: block;
-        color: #000;
-    }
-
+  a {
+    text-decoration: none;
+    display: block;
+    color: #000;
+  }
 `
 export default GlobalStyle;
