a387ce474d1c443b5eca16159489f7141351f23d
dihoon <dihoon955@gmail.com>
2022-12-15 17:25:51 +0900

    Feat: 무한 스크롤 구현

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,22 +1,65 @@
-import getAllFarmData from '../lib/getAllFarmData';
+// import getAllFarmData from '../lib/getAllFarmData';
 import ItemList from '../components/itemList';
 import {useState, useEffect} from 'react';
 
+import InfiniteScroll from 'react-infinite-scroll-component';
+import axios from 'axios';
+
+const NUMBER_OF_ITEMS_TO_GET = 10;
+
 const Home = () => {
 
+  const [currentIndex, setCurrentIndex] = useState(0);
   const [contents, setContents] = useState([]);
 
+  console.log(contents);
+
   useEffect(()=>{
-    getAllFarmData().then(res=>{
-      setContents(res);
+    getData();
+  },[])
+
+  const getData = async () => {
+    axios.get('mock_data/farms.json').then(res=>{
+      return res.data;
+    }).then(res=>{
+      const data = res.slice(currentIndex, currentIndex+NUMBER_OF_ITEMS_TO_GET);
+      
+      setContents(contents.concat(data));
     })
-  }, [])
+    setCurrentIndex(currentIndex + NUMBER_OF_ITEMS_TO_GET);
+  }
 
   return (
     <>
-      <ItemList contents={contents}/>
+      <InfiniteScroll 
+      dataLength={contents.length}
+      next={getData}
+      hasMore={true}>
+        <ItemList contents={contents}/>
+      </InfiniteScroll>
     </>
-  );
-};
+  )
+}
 
 export default Home;
+
+// const Home = () => {
+
+//   const [ref, inView] = useInView();
+//   const [contents, setContents] = useState([]);
+
+//   useEffect(()=>{
+//     getAllFarmData().then(res=>{
+//       setContents(res);
+//     })
+//   }, [])
+
+//   return (
+//     <>
+//       <ItemList contents={contents}/>
+//     </>
+//   );
+// };
+
+// export default Home;
+
