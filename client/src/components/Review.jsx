611b5b655ae4ea6cab8a55a1f73c5728feebd563
Park Ji Hye <qkrwlgp235@gmail.com>
2022-12-16 11:14:59 +0900

    Remove : Calender

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
 import styled from 'styled-components';
-import Modal from "../components/modal";
+import Modal from "../components/Modal";
 
 
 const ReviewItems = ({review,all}) =>{
