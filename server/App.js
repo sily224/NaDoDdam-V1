b2549a71f6075ad00f485f137a8347001c4d98c3
zuhye <mari5669@naver.com>
2022-12-16 10:50:25 +0900

    rename: 파일명 변경 및 삭제 !!

--- server/App.js
+++ server/App.js
@@ -1,10 +1,8 @@
 import cors from "cors";
 import express from "express";
 import bodyParser from "body-parser";
-// import sequelize from("./models");
 import db from "./models/index.js";
-import router from "./router/user-router.js";
-// import { userRouter } from "./contoller/auth.js";
+import authRouter from "./router/auth.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -21,15 +19,7 @@ app.get('/', (req, res) => {
     res.send('Server Response Success');
 })
 
-app.use("/api", router);
-
-// sequelize.sync({force: false})
-// .then(()=> {
-//   console.log("데이터베이스 연결 성공 !!")
-// })
-// .catch((err)=> {
-//   console.error(err);
-// });
+app.use("/api", authRouter);
 
 db.sequelize
   .sync()
