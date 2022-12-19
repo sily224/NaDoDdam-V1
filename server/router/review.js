78a062a5ebfc308f19671642f9fc5c716d9f2ce1
zuhye <mari5669@naver.com>
2022-12-19 16:21:29 +0900

    feat: Reservation 수정 & Review CRD
    Reservation 본인 예약 조회 기능과 Review 생성, 삭제, 모두 조회, 개인
    리뷰 조회 구현

---
+++ server/router/review.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import 'express-async-errors';
+import * as reviewController from '../controller/review.js';
+import { isAuth } from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.post('/review', isAuth, reviewController.review); //리뷰 등록
+
+router.get('/reviewlist', reviewController.reviewList); // 모든 리뷰 정보 조회
+
+router.get('/review/:id', isAuth, reviewController.getReveiwData); // 본인 리뷰 조회
+
+router.delete('/review/:id', isAuth, reviewController.reviewDrop); //리뷰 삭제
+
+// router.put('/review/:id',isAuth,  reviewController.reserveUpdate); //리뷰 수정
+
+export default router;
