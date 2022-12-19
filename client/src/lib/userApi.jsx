774a8d6b8ab7d07219d16b5071a8c2b39b8733bc
Jiwoo <wldn0804@gmail.com>
2022-12-19 19:41:13 +0900

    fix: 로그인, 회원가입 보완

--- client/src/lib/userApi.jsx
+++ client/src/lib/userApi.jsx
@@ -1,45 +1,37 @@
-import axios from "axios";
+import axios from 'axios';
 // api 로 POST 요청 (/endpoint 로, JSON 데이터 형태로 요청함)
 // endpoint : api/user/login
 async function post(endpoint, data) {
-  console.log("post 들어옴");
-  const apiUrl = endpoint;
+	const apiUrl = endpoint;
+	const bodyData = JSON.stringify(data);
+	console.log(`%cPOST 요청: ${apiUrl}`, 'color: #296aba;');
+	console.log(`%cPOST 요청 데이터: ${bodyData}`, 'color: #296aba;');
 
-  const bodyData = JSON.stringify(data);
-  // console.log(`%cPOST 요청: ${apiUrl}`, "color: #296aba;");
-  // console.log(`%cPOST 요청 데이터: ${bodyData}`, "color: #296aba;");
+	const res = await axios(apiUrl, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${localStorage.getItem('token')}`,
+		},
+		data: bodyData,
+	});
 
-  const res = await axios(apiUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-    data: bodyData,
-  });
-
-  // 응답 코드가 4XX 계열일 때 (400, 403 등)
-  // if (!res.ok) {
-  //   console.log("에러 답변", res);
-  //   throw new Error(res);
-  // }
-
-  return res;
+	return res;
 }
 
-// api 로 user정보 GET 요청 
+// api 로 user정보 GET 요청
 async function get(endpoint) {
-  const apiUrl = endpoint;
-  console.log(`%cGET 요청: ${apiUrl} `, "color: #a25cd1;");
+	const apiUrl = endpoint;
+	console.log(`%cGET 요청: ${apiUrl} `, 'color: #a25cd1;');
 
-  const res = await axios(apiUrl, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-  });
+	const res = await axios(apiUrl, {
+		method: 'GET',
+		headers: {
+			Authorization: `Bearer ${localStorage.getItem('token')}`,
+		},
+	});
 
-  return res;
+	return res;
 }
 
 export { post, get };
