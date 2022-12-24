11f0b7c61318862a58f10db5c694daee9769d6f6
pilsungchoi <vlftjd159753@gmail.com>
2022-12-25 02:18:37 +0900

    Feature : 타임테이블 이미지data

--- server/models/timetable.js
+++ server/models/timetable.js
@@ -90,6 +90,19 @@ const TimeTables = (sequelize, DataTypes) => {
 	timeTable.getTimeTables = (farmId, lastId, limit) => {
 		let cursor = lastId || 0;
 		return timeTable.findAll({
+			attributes: [
+				'id',
+				'date',
+				'personnel',
+				'price',
+				'start_time',
+				'end_time',
+				[DataTypes.col('farm.url'), 'url'],
+			],
+			include: {
+				model: db.Farms,
+				attributes: [],
+			},
 			limit: parseInt(limit),
 			where: {
 				farmId,
