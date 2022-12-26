74d5b553c9ce6326302f5fa7452e9e7e8062e049
zuhye <mari5669@naver.com>
2022-12-26 18:10:58 +0900

    fix: error-handler

--- server/router/likeFarms.js
+++ server/router/likeFarms.js
@@ -2,13 +2,14 @@ import express from 'express';
 import 'express-async-errors';
 import * as likeFarmsController from '../controller/likeFarms.js';
 import { isAuth } from '../middleware/auth.js';
+import { errorHandler } from '../middleware/error-handler.js';
 
 const router = express.Router();
 
-router.post('/like/:farmId', isAuth, likeFarmsController.like); //찜 등록
+router.post('/like/:farmId', isAuth, likeFarmsController.like, errorHandler); //찜 등록
 
-router.get('/like', isAuth, likeFarmsController.getLikeData); // 본인 찜 조회
+router.get('/like', isAuth, likeFarmsController.getLikeData, errorHandler); // 본인 찜 조회
 
-router.delete('/like/:farmId', isAuth, likeFarmsController.likecancel); //찜 삭제
+router.delete('/like/:farmId', isAuth, likeFarmsController.likecancel, errorHandler); //찜 삭제
 
 export default router;
