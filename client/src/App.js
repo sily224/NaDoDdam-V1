5b738b38850fc8011a6f96f1b8b877ae872a1de2
Young-yoii <fhqht4545@gmail.com>
2022-12-27 16:10:03 +0900

    Feat: 리뷰 수정, 삭제 기능 구현 완료

--- client/src/App.js
+++ client/src/App.js
@@ -94,7 +94,7 @@ function App() {
 						}
 					/>
 					<Route
-						path="/myreview/updateeview/:id"
+						path="/updatereview/:id"
 						element={
 							<MemberRoute>
 								<UpdateReviewPage />
