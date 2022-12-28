7496620bf3d64b34bd338bb07c4516a9543524c3
Park Ji Hye <qkrwlgp235@gmail.com>
2022-12-28 13:05:41 +0900

    Styles : 페이지네이션 액티브색상 적용

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -2,7 +2,13 @@ import {PageWrapper, PageBtn} from '../styles/Styled'
 
 // memo 지우: total(데이터 총 갯수), limit(한 페이지에 보여줄 갯수)
 const Pagination = ({ total, limit, page, setPage }) => {
+	const [btnActive, setBtnActive] = useState("");
 	const numPages = Math.ceil(total / limit);
+
+    const hadlePageBtn = (e,i) => {
+        setPage(i+1);
+        setBtnActive(e.target.value);
+    };
 
 	return (
 		<>
@@ -13,9 +19,12 @@ const Pagination = ({ total, limit, page, setPage }) => {
 				{Array(numPages)
 					.fill()
 					.map((_, i) => (
+
 						<PageBtn
+						value={i}
 							key={i + 1}
-							onClick={() => setPage(i + 1)}
+							className={i == btnActive ? "active" : ""}
+							onClick={(e) => hadlePageBtn(e,i)}
 							aria-current={page === i + 1 ? 'page' : null}
 						>
 							{i + 1}
